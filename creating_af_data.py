afc07d3426ab850803aac511c98a2e4cd0f62e38
Karolina Walędzik <karaleina@gmail.com>
2017-09-25 21:14:38 +0200

    created first af database version

--- creating_af_data.py
+++ creating_af_data.py
@@ -1,18 +1,40 @@
 from AF.parsers import ecg_recording_parser_af
 from matplotlib import pyplot as plt
-
-from AF.simple_medical_analysers import wavelet_analysis
-import numpy as np
-from os import path
-import wfdb
-
-
 from os import path
 from matplotlib import pyplot as plt
+import numpy as np
+import pprint, pickle
+from AF.analyzers import bothChannelsQRSDetector, RRIntervalsAnalyser
+from AF.simple_medical_analysers import wavelet_analysis
+import wfdb
+from os import path
 from AF.parsers import ecg_recording_parser_af
 
-import wfdb
-import numpy as np
+
+def get_list_of_intervals(signals_af):
+    """Getting RR signals and returning list of intervals"""
+
+    # Wavelet filtration
+    numbers_of_signals = len(signals_af[0, :])
+    numbers_of_samples = len(signals_af[:, 0])
+
+    for i in range(numbers_of_signals):
+        if numbers_of_samples % 2 == 1:
+            signals_af = signals_af[:numbers_of_samples - 1, :]
+        signals_af[:, i] = wavelet_analysis.filter_signal(signals_af[:, i], wavelet="db6", highcut=False)
+
+    # Finding R-waves
+    analyzerQRS = bothChannelsQRSDetector.BothChannelsQRSDetector()
+    combined_rr = analyzerQRS.compare(signals_af, record_file=file_path, plotting=False, qrs_reading=False)
+
+    # Creating list of isoelectrics lines
+    rr_ia = RRIntervalsAnalyser.RRIntervalsAnalyser(analyzerQRS)
+    list_of_intervals_chann0, rr_distances_chann0 = rr_ia.get_intervals(combined_rr, channel_no=0,
+                                                                        time_margin=0.1)
+    list_of_intervals_chann1, rr_distances_chann1 = rr_ia.get_intervals(combined_rr, channel_no=1,
+                                                                        time_margin=0.1)
+
+    return (list_of_intervals_chann0, list_of_intervals_chann1)
 
 file_with_all_names = path.join("downloads", "records_names_af")
 file_object = open(file_with_all_names, "r")
@@ -20,7 +42,12 @@ file_object = open(file_with_all_names, "r")
 afib_parsed_number = 0
 
 for file_no in file_object:
+    # if file_no < "07162":
+    #     continue
 
+    dataset_per_file_no = {
+                "channel0": [],
+               "channel1": []}
     try:
         file_no = file_no.replace("\n", "")
         file_path = path.join("downloads", "af", file_no)
@@ -36,9 +63,24 @@ for file_no in file_object:
                 if s - 1000 < 0 :
                     start = s
                 parser = ecg_recording_parser_af.ECGRecordingDataParser()
+
                 signals_af = np.array(parser.parse(dat_file_name=file_path
                                     + ".dat", from_sample=start, to_sample=s+1000))
                 afib_parsed_number += 1
+
+                (list_of_intervals_chann0, list_of_intervals_chann1) = get_list_of_intervals(signals_af)
+
+                # Uptadting dataset
+                if len(list_of_intervals_chann1) == len(list_of_intervals_chann0):
+                    # Saving isoelectric channel
+                    dataset_per_file_no["channel0"] += list_of_intervals_chann0
+                    dataset_per_file_no["channel1"] += list_of_intervals_chann1
+                else:
+                    print("SUMA KONTROLNA SIĘ NIE ZGADZA!!!!!!! :(((( ")
+
+        output = open('database/af_data/' + file_no + '.pkl', 'wb')
+        pickle.dump(dataset_per_file_no, output, -1)
+        output.close()
 
     except FileNotFoundError:
         print("Brak kompletu plików dla rekordu o nr: " + str(file_no))
