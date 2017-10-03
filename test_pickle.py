d92a8aa4848e4537699db86b1cb254b3bd0e3866
Karolina Walędzik <karaleina@gmail.com>
2017-10-03 23:04:49 +0200

    Added pickle third orrection

--- test_pickle.py
+++ test_pickle.py
@@ -7,19 +7,19 @@ from AF.parsers import ecg_recording_parser_af
 from AF.analyzers import bothChannelsQRSDetector, RRIntervalsAnalyser
 from AF.simple_medical_analysers import wavelet_analysis
 
-pkl_file_no = "07910"
+pkl_file_no = "08455"#"07910"
 
 
-new_pickle_dir = 'database/af_corrected_data/'
-filepath_new_pickle = path.join(new_pickle_dir, str(pkl_file_no) + '.pkl')
+new_pickle_dir = 'database/af_corrected2_data/'
+filepath_old_pickle = path.join(new_pickle_dir, str(pkl_file_no) + '.pkl')
 
-pkl_file = open(filepath_new_pickle, 'rb')#open('database/af_data/' + str(pkl_file_no) + '.pkl', 'rb')
+pkl_file = open(filepath_old_pickle, 'rb')#open('database/af_data/' + str(pkl_file_no) + '.pkl', 'rb')
 dataset = pickle.load(pkl_file)
 pkl_file.close()
 
 # filepath_new_pickle = path.abspath(filepath_new_pickle)
 
-print(filepath_new_pickle)
+print(filepath_old_pickle)
 
 #print(dataset)
 
@@ -27,10 +27,15 @@ plt.close("all")
 print("len", len(dataset["channel1"]))
 
 
+new_choosen_dataset = dataset.copy()
+new_choosen_dataset["channel1"] = []
+new_choosen_dataset["channel0"] = []
+
+
 def start_looping(index=0):
     for i in range(len(dataset["channel1"])):
 
-            if i >= index:
+            if i >= 0:
                 plt.ion()
                 plt.figure(0).clear()
                 plt.title(str(i))
@@ -53,15 +58,22 @@ def start_looping(index=0):
                 if input_text == "save":
                     print("Zażądano zapisania danych do pliku" + str(pkl_file))
 
+                    filepath_new_pickle = path.join(new_pickle_dir, "new_" + str(pkl_file_no) + '.pkl')
                     output = open(filepath_new_pickle, 'wb')
-                    pickle.dump(dataset, output, -1)
+                    pickle.dump(new_choosen_dataset, output, -1)
                     output.close()
+
+                if input_text == "y":
+                    new_choosen_dataset["channel1"].append(dataset["channel1"][i])
+                    new_choosen_dataset["channel0"].append(dataset["channel0"][i])
+                    print("len of new dataset ", len(new_choosen_dataset["channel0"]))
 
                 plt.pause(0.05)
 start_looping(0)
 
+filepath_new_pickle = path.join(new_pickle_dir, "new_" + str(pkl_file_no) + '.pkl')
 output = open(filepath_new_pickle, 'wb')
-pickle.dump(dataset, output, -1)
+pickle.dump(new_choosen_dataset, output, -1)
 output.close()
 
 
