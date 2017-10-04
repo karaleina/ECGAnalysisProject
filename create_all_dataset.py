f15ce0fca8c58d15ce07d49c6899bfa28db9f102
Karolina Walędzik <karaleina@gmail.com>
2017-10-04 12:19:48 +0200

    Added first all_dataset

--- create_all_dataset.py
+++ create_all_dataset.py
@@ -1,6 +1,8 @@
 #shape(6, N)
 from os import path
 import pickle
+from matplotlib import pyplot as plt
+
 
 def create_af_dataset():
     file_with_all_names = path.join("downloads", "records_names_af")
@@ -61,4 +63,57 @@ def create_ptb_dataset():
 
 all_af_dataset = create_af_dataset()
 all_ptb_dataset = create_ptb_dataset()
+all_dataset = {"channel0" : [],
+               "channel1" : [],
+               "diagnose" : [] # 1 af, 0 norm
+}
+
+for i in range(len(all_af_dataset["channel0"])):
+
+    if all_af_dataset["channel0"][i].get_signal() != None and all_af_dataset["channel0"][i].get_signal() != None:
+        all_dataset["channel0"].append(all_af_dataset["channel0"][i])
+        all_dataset["channel1"].append(all_af_dataset["channel1"][i])
+        all_dataset["diagnose"].append(1)
+
+    if all_ptb_dataset["channel0"][i].get_signal() != None and all_ptb_dataset["channel0"][i].get_signal() != None:
+        all_dataset["channel0"].append(all_ptb_dataset["channel0"][i])
+        all_dataset["channel1"].append(all_ptb_dataset["channel1"][i])
+        all_dataset["diagnose"].append(0)
+
+        # plt.ion()
+        # plt.figure(2)
+        # plt.subplot(2,1,1).cla()
+        # plt.plot(all_ptb_dataset["channel0"][i].get_signal())
+        # plt.subplot(2,1,2).cla()
+        # plt.plot(all_ptb_dataset["channel1"][i].get_signal())
+        # plt.pause(0.05)
+
+        #         plt.ion()
+        # plt.figure(1)
+        # plt.subplot(2,1,1).cla()
+        # plt.plot(all_af_dataset["channel0"][i].get_signal())
+        # #
+        # # plt.subplot(2,1,2).cla()
+        # # plt.plot(all_af_dataset["channel1"][i].get_signal())
+        # #
+        # # plt.figure(2)
+        # # plt.subplot(2,1,1).cla()
+        # # plt.plot(all_ptb_dataset["channel0"][i*2].get_signal())
+        # # plt.subplot(2,1,2).cla()
+        # # plt.plot(all_ptb_dataset["channel1"][i * 2].get_signal())
+        # #
+        # # plt.pause(0.1)
+
+diagnose_list = all_dataset["diagnose"]
+print(diagnose_list)
+indices_of_af = [i for i, x in enumerate(diagnose_list) if x == 1]
+indices_of_ptb = [i for i, x in enumerate(diagnose_list) if x == 0]
+print(len(indices_of_af))
+print(len(indices_of_ptb))
+print(len(diagnose_list))
+
+
+
+
+
 
